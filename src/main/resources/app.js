a63986d771b455d4eec4aa0b7d79cad9b92d3d8f
PUNI-NR <173622702+puninrkhola@users.noreply.github.com>
2025-04-22 14:44:41 +0530

    Initial project setup with pom.xml and resources

---
+++ src/main/resources/app.js
@@ -0,0 +1,16 @@
+function validateLogin()
+{
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username === "admin" && password === "1234")
+    {
+        window.location.href = "home.html";
+        //return false;
+    }
+    else{
+        alert("invalid username or password");
+        //return false;
+    }
+}
+
